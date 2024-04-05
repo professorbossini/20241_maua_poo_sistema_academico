779040dec58b21f4241b2574b077140475045a7e
Rodrigo Bossini <professorbossini@gmail.com>
2024-04-05 11:33:01 -0300

    chore(sist-acad): cria o projeto

---
+++ src/main/java/br/com/bossini/_maua_poo_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.bossini._maua_poo_sistema_academico;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
